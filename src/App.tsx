efed13b8a7ce91582caba774cb31f360fcb43ca9
Aleksey Savchenko <wasxela@gmail.com>
2022-08-21 18:53:34 +0300

    remove Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Logo from './components/Logo/Logo';
 
 const App = (): JSX.Element => {
-  return <div className='App'>Hello React</div>;
+  return (
+    <div className='App'>
+      <Header />
+      <main>
+        <Logo type='header' />
+        <Logo type='footer' />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
