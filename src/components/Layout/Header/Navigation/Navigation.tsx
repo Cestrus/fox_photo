85325883065cc2de7d342bb1e2f7d681e35d264b
Aleksey Savchenko <wasxela@gmail.com>
2022-08-25 22:49:31 +0300

    created Navigation component

--- src/components/Layout/Header/Navigation/Navigation.tsx
+++ src/components/Layout/Header/Navigation/Navigation.tsx
@@ -1,9 +1,45 @@
+import cn from 'classnames';
 import React from 'react';
 
 import styles from './Navigation.module.css';
 
 const Navigation: React.FC = (props) => {
-  return <nav className={styles.nav}></nav>;
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <div className={styles.wrap}>
+            <a href='#'>
+              <p>Виды печати</p>
+            </a>
+          </div>
+        </li>
+        <li>
+          <div className={styles.wrap}>
+            <a href='#'>
+              <p>Наши работы</p>
+            </a>
+          </div>
+        </li>
+        <li>
+          <div className={styles.wrap}>
+            <a href='#'>
+              <p>Контакты</p>
+            </a>
+          </div>
+        </li>
+      </ul>
+      <div className={styles.wrapLast}>
+        <div className={styles.last}>
+          <a href='#'>
+            <p>
+              Конструктор<span> футболок</span>
+            </p>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
