d802923fc9d238d34b9fd452bf6226f382b6ed4e
Aleksey Savchenko <wasxela@gmail.com>
2022-08-27 21:34:47 +0300

    created footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
 
+import styles from './Footer.module.css';
+
+import Logo from '../../Logo/Logo';
+
 export const Footer: React.FC = (props) => {
-  return <></>;
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <Logo type='footer' />
+      <p className={styles.copyright}>© FoxPhoto, {year}. Все права защищены</p>
+      <div className={styles.links}>
+        <ul>
+          <li>
+            <a href='#'>Виды печати</a>
+          </li>
+          <li>
+            <a href='#'>Наши работы</a>
+          </li>
+          <li>
+            <a href='#'>Цены</a>
+          </li>
+          <li>
+            <a href='#'>Контакты</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
